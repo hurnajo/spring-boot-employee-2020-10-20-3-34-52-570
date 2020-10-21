b16ff01a38b9b1e078b1e10ddeb24da6a2b2d08f
HURNAJO <jose.leo.hurna@oocl.com>
2020-10-21 21:28:53 +0800

    [Restful API][Leo and Prince] Feature - Create logic for findByGender.

--- src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -35,6 +35,6 @@ public class EmployeeService {
     }
 
     public List<Employee> findByGender(String gender) {
-        return null;
+        return employeeRepository.findByGender(gender);
     }
 }
