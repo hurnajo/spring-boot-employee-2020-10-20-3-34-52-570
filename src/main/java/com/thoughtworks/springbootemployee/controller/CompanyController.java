0d7a4daf875e702e5cf3e36809633837aef0f9f4
HURNAJO <jose.leo.hurna@oocl.com>
2020-10-21 23:56:51 +0800

    [Resful API][Prince and Leo] Feature - add GetMapping getAll for CompanyController

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -0,0 +1,25 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.service.CompanyService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/companies")
+public class CompanyController {
+    private final CompanyService companyService;
+
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @GetMapping
+    public List<Company> getAll() {
+        return companyService.getAll();
+    }
+    
+}
