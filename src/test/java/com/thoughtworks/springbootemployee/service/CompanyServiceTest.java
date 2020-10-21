4aba6001242a08f974b5cb71d47ab1a9e7725db7
HURNAJO <jose.leo.hurna@oocl.com>
2020-10-21 23:45:52 +0800

    [Restful API][Leo and Prince] Refactor - change implementation for all test case

--- src/test/java/com/thoughtworks/springbootemployee/service/CompanyServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/CompanyServiceTest.java
@@ -3,19 +3,19 @@ package com.thoughtworks.springbootemployee.service;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
-import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.Collections;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
-public class CompanyServiceTest {
-    CompanyRepository repository = Mockito.mock(CompanyRepository.class);
+class CompanyServiceTest {
+    private CompanyRepository repository = Mockito.mock(CompanyRepository.class);
 
     @Test
     void should_get_all_when_get_company() {
@@ -34,9 +34,8 @@ public class CompanyServiceTest {
     @Test
     void should_create_company_when_create_given_company() {
         //given
-        List<Employee> employees = asList(new Employee(1, "Leo", 18, "male", 1000),
-            new Employee(2, "Leo", 18, "male", 1000));
-        Company company = new Company(1,"OOCL", 2, employees);
+
+        Company company = new Company(1, "OOCL");
         CompanyService service = new CompanyService(repository);
         when(repository.save(company)).thenReturn(company);
         //when
@@ -48,56 +47,51 @@ public class CompanyServiceTest {
     @Test
     void should_get_company_when_get_by_name_given_company_id() {
         //given
-        List<Employee> employees = asList(new Employee(1, "Leo", 18, "male", 1000),
-                new Employee(2, "Leo", 18, "male", 1000));
-        Company company = new Company(1,"OOCL", 2, employees);
-        when(repository.findById(company.getId())).thenReturn(company);
+        Company company = new Company(1, "OOCL");
+        when(repository.findCompanyById(company.getCompanyId())).thenReturn(company);
         CompanyService service = new CompanyService(repository);
         //when
-        Company actual = service.findById(company.getId());
+        Company actual = service.findById(company.getCompanyId());
         //then
-        assertEquals(company.getId(),actual.getId());
+        assertEquals(company.getCompanyId(), actual.getCompanyId());
     }
 
     @Test
     void should_get_list_of_employee_when_search_given_certain_company() {
         //given
-        List<Employee> employees = asList(new Employee(1, "Leo", 18, "male", 1000),
-                new Employee(2, "Leo", 18, "male", 1000));
-        Company company = new Company(1,"OOCL", 2, employees);
-        CompanyService service = new CompanyService(repository);
+        Company company = new Company(1, "Telus");
+        Employee employee = new Employee(1, "Tom", 18, "Male", 1000, 1);
+
+        when(repository.getEmployeesByCompanyId(company.getCompanyId())).thenReturn(Collections.singletonList(employee));
+        CompanyService companyService = new CompanyService(repository);
         //when
-        List<Employee> actual = service.getEmployeeByCompanyId(company.getId());
+        List<Employee> actual = companyService.getEmployeesByCompanyId(company.getCompanyId());
         //then
-        assertEquals(employees,actual);
+        assertTrue(actual.contains(employee));
     }
 
     @Test
     void should_update_company_when_update_by_company_id_given_company_id() {
         //given
-        List<Employee> employees = asList(new Employee(1, "Leo", 18, "male", 1000),
-                new Employee(2, "Leo", 18, "male", 1000));
-        Company company = new Company(1,"OOCL", 2, employees);
+        Company company = new Company(1, "OOCL");
+        Company updateCompany = new Company(1, "COSCO");
 
-        Company updateCompany = new Company(1,"COSCO",2, employees);
-        when(repository.updateById(company.getId(),updateCompany)).thenReturn(updateCompany);
+        when(repository.updateCompanyById(company.getCompanyId(), updateCompany)).thenReturn(updateCompany);
         CompanyService service = new CompanyService(repository);
         //when
-        Company actual = service.updateById(company.getId(),updateCompany);
+        Company actual = service.updateById(company.getCompanyId(), updateCompany);
         //then
-        assertEquals("COSCO",actual.getCompanyName());
+        assertEquals("COSCO", actual.getCompanyName());
     }
 
     @Test
     void should_delete_company_when_delete_by_company_id_given_company() {
         //given
-        List<Employee> employees = asList(new Employee(1, "Leo", 18, "male", 1000),
-                new Employee(2, "Leo", 18, "male", 1000));
-        Company company = new Company(1,"OOCL", 2, employees);
+        Company company = new Company(1, "OOCL");
         CompanyService service = new CompanyService(repository);
         //when
-        service.deleteById(company.getId());
+        service.deleteById(company.getCompanyId());
         //then
-        verify(repository,times(1)).deleteById(company.getId());
+        verify(repository, times(1)).deleteById(company.getCompanyId());
     }
 }
